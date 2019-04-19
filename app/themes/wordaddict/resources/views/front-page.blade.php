9b967de517ca5de8b7c0b6683bb403b2bdd9473b
Filip Ruzicka <f@filipruzicka.com>
2019-04-19 20:18:52 +0200

    Add Front page Donate bar

--- app/themes/wordaddict/resources/views/front-page.blade.php
+++ app/themes/wordaddict/resources/views/front-page.blade.php
@@ -1,10 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-  
+  @include ('partials.donate-bar')
   @include('partials.current-issue')
   @include('partials.wordaddict-about')
   @include('partials.past-issues')
   @include('partials.newsletter')
-  
 @endsection
---
+++ app/themes/wordaddict/resources/views/partials/donate-bar.blade.php
@@ -0,0 +1,12 @@
+<div class="donatebar">
+    <div class="donatebar-content">
+      <h3 class="donatebar-heading">needs your help.</h3>
+      <p>As we near into the release of one of the most anticipated issues of the Word Addict, we are facing some upfront costs. In order to secure a good print and a launch event that is worthy of skipping your other favorite activities, we need cash for everything to go smoothly.</p>
+      <div class="donatebar-donate">
+        <p class="donatebar-donate-text">Please check out what's in it for you and why do we need this. Thank you.</p>
+        <a href="#" class="donatebar-donate-button">
+          <span class="donatebar-donate-button-insert">Fund the next issue</span>
+        </a>
+      </div>
+    </div>
+</div>
